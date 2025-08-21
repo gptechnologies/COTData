14fb6a14919c0225770473a40fa9fe8e6d0a0f38
gptechnologies <jmangat@gptechnologies.ai>
2025-08-20 21:33:35 -0400

    Delete cot-viz-vercel directory

--- cot-viz-vercel/app/components/Plot.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import createPlotlyComponent from "react-plotly.js/factory";
-import Plotly from "plotly.js-basic-dist-min";
-
-const Plot = createPlotlyComponent(Plotly);
-export default Plot;
